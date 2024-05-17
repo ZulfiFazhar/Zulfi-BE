480200b2d5f9f9f906f3491efe1663f3ca3eb100
Zulfi Fadilah Azhar <zulfi.fazhar12@gmail.com>
2024-05-17 17:11:01 +0700

    Refactor: UserJson req.body

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,11 +1,11 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const userController = require('../controllers/userController');
+const userController = require("../controllers/userController");
 
-router.post('/create', userController.createUser);
-router.get('/read/all', userController.getAllUsers);
-router.get('/read/:id', userController.getUserById);
-router.post('/update/', userController.updateUserById);
-router.delete('/delete/:id', userController.deleteUserById);
+router.post("/create", userController.createUser);
+router.get("/read/all", userController.getAllUsers);
+router.get("/read/:id", userController.getUserById);
+router.post("/update/", userController.updateUserById);
+router.delete("/delete/:id", userController.deleteUserById);
 
 module.exports = router;
